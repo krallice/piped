028c0a15ce78d22feda21abdcaa5c3c2075ae2d6
root <root@zhost1.smithfield>
2016-02-15 04:04:24 -0500

    Scraping URLs and Names

--- piped.py
+++ piped.py
@@ -2,21 +2,35 @@
 
 # Imports:
 import yaml
+from lxml import html
+import requests
 
 # Our load config function:
 def loadConfig():
+
   with open("config.yaml", "r") as f:
     config = yaml.load(f)
     return config
 
 def pipedMain():
 
-  # Load up our config variable
+  debugMode = 1
   config = loadConfig()
 
+  # Loop over each pipe brand:
   for brand in config["pipeBrands"]:
+
     parentURL = config["baseURL"] + brand + "/"
-    print parentURL
+    if debugMode == 1:
+      print "Fetching " + parentURL
+    parentHTML = requests.get(parentURL)
+    parentTree = html.fromstring(parentHTML.content)
+
+    # Locate the 'NEW' spans:
+    newURLs = parentTree.xpath('//span[@class="new"]/following-sibling::a/@href')
+    newNames = parentTree.xpath('//span[@class="new"]/following-sibling::a/text()')
+    print newURLs
+    print newNames
 
 # Call pipedMain on program entry:
 if __name__ == "__main__":
