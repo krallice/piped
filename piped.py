44e1968cc8ba80f3ac3162708c1e6f06b342d5d2
root <root@zhost1.smithfield>
2016-02-16 07:36:06 -0500

    Adds cache checking functionality

--- piped.py
+++ piped.py
@@ -11,12 +11,19 @@ class Pipe(object):
   def __init__(self, url, description):
     self.url = url
     self.description = description
+    self.newPipe = True
 
   def get_url(self):
     return self.url
 
   def get_description(self):
     return self.description
+
+  def get_newStatus(self):
+    return self.newStatus
+
+  def set_oldPipe(self):
+    self.newStatus = False
 
 # Our load config function:
 def loadConfig():
@@ -25,12 +32,15 @@ def loadConfig():
     config = yaml.load(f)
     return config
 
+# Our Main Function:
 def pipedMain():
 
   debugMode = 1
-  config = loadConfig()
 
-  # Loop over each pipe brand:
+  config = loadConfig()
+  pipeList = []
+
+  # Loop over each pipe brand, and populate our Pipe class:
   for brand in config["pipeBrands"]:
 
     parentURL = config["baseURL"] + brand + "/"
@@ -43,11 +53,35 @@ def pipedMain():
     newURLs = parentTree.xpath('//span[@class="new"]/following-sibling::a/@href')
     newDescriptions = parentTree.xpath('//span[@class="new"]/following-sibling::a/text()')
 
-    pipeList = []
-
+    # Populate the Pipe class:
     for i in range(len(newURLs)):
       pipe = Pipe(newURLs[i], newDescriptions[i])
+      pipeList.append(pipe)
       print pipe.get_url()
+
+  # Open our cache file for reading:
+  with open(config["cacheFile"], "a+") as cf:
+
+    if debugMode == 1:
+      print "Reading Cache File"
+
+    # Read file into string (scaling should be ok):
+    cache = cf.readlines()
+
+    # Compare our cache with our pipeList:
+    for pipe in pipeList:
+      for line in cache:
+        if pipe.get_url() == line.rstrip():
+          pipe.set_oldPipe()   
+          if debugMode == 1:
+            print "Pipe " + pipe.get_description() + " is OLD!"
+
+    # Generate Notification of new pipes + update our cache:
+    if debugMode == 1:
+      print "Generating Email now and updating cache:"
+    for pipe in pipeList:
+      if pipe.get_newStatus() == True:
+      # todo: generate notification + update cache 
 
 # Call pipedMain on program entry:
 if __name__ == "__main__":
