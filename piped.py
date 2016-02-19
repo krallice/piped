0386b4fbd9f3accc28b81c044411be9935cd0267
krallice <krallice@github.com>
2016-02-20 10:34:47 +1100

    (Cache File, Private/Public YAML, Mail through Sendmail)

--- piped.py
+++ piped.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 # Imports:
+import os
 import yaml
 from lxml import html
 import requests
@@ -8,31 +9,66 @@ import requests
 # Class definition for our pipe object:
 class Pipe(object):
 
-  def __init__(self, url, description):
+  def __init__(self, url, brand, description):
     self.url = url
+    self.brand = brand
     self.description = description
     self.newPipe = True
 
   def get_url(self):
     return self.url
 
+  def get_brand(self):
+    return self.brand
+
+  def get_pretty_brand(self):
+    return self.brand.title()
+
   def get_description(self):
     return self.description
 
   def get_newStatus(self):
-    return self.newStatus
+    return self.newPipe
 
   def set_oldPipe(self):
-    self.newStatus = False
+    self.newPipe = False
 
 # Our load config function:
 def loadConfig():
 
   with open("config.yaml", "r") as f:
-    config = yaml.load(f)
-    return config
+    openConfig = yaml.load(f)
 
-# Our Main Function:
+  with open("privateconfig.yaml", "r") as f:
+    privateConfig = yaml.load(f)
+
+  config = openConfig.copy()
+  config.update(privateConfig)
+
+  return config
+
+# Send Basic Email via sendmail, perhaps oneday replace 
+# with a nice MIME templated email:
+def sendBasicMail(newPipes, senderAddress, destinationEmails, domainTop):
+
+  # Leverage sendmail binary:
+  sendmail_location = "/usr/sbin/sendmail"
+  p = os.popen("%s -t" % sendmail_location, "w")
+  p.write("From: %s\n" % senderAddress)
+  p.write("To: %s\n" % destinationEmails)
+  p.write("Subject: New Pipes!\n")
+  p.write("Content-Type: text/html")
+  p.write("\n") # blank line separating headers from body
+  p.write("<table border=1>")
+  for pipe in newPipes:
+    fullPath=domainTop + pipe.get_url()
+    p.write("<tr>")
+    p.write("<td><a href='%s'>%s</a></td>\n" % ( fullPath, pipe.get_description()))
+    p.write("<td>%s</td>\n" % pipe.get_pretty_brand())
+    p.write("</tr>")
+  p.write("</table>")
+  status = p.close()
+
 def pipedMain():
 
   debugMode = 1
@@ -55,11 +91,11 @@ def pipedMain():
 
     # Populate the Pipe class:
     for i in range(len(newURLs)):
-      pipe = Pipe(newURLs[i], newDescriptions[i])
+      pipe = Pipe(newURLs[i], brand, newDescriptions[i])
       pipeList.append(pipe)
       print pipe.get_url()
 
-  # Open our cache file for reading:
+  # Open our cache file:
   with open(config["cacheFile"], "a+") as cf:
 
     if debugMode == 1:
@@ -68,6 +104,8 @@ def pipedMain():
     # Read file into string (scaling should be ok):
     cache = cf.readlines()
 
+    newPipes = []
+
     # Compare our cache with our pipeList:
     for pipe in pipeList:
       for line in cache:
@@ -75,13 +113,17 @@ def pipedMain():
           pipe.set_oldPipe()   
           if debugMode == 1:
             print "Pipe " + pipe.get_description() + " is OLD!"
-
-    # Generate Notification of new pipes + update our cache:
-    if debugMode == 1:
-      print "Generating Email now and updating cache:"
-    for pipe in pipeList:
+      # Our pipe is new?
       if pipe.get_newStatus() == True:
-      # todo: generate notification + update cache 
+        # Update our cache:
+        cf.write(pipe.get_url() + "\n")
+        newPipes.append(pipe)
+
+    # Check if we actually have new pipes:
+    if len(newPipes) > 1:
+      if debugMode == 1:
+        print "New Pipes Exist -- Sending Email"
+      sendBasicMail(newPipes, config["senderAddress"], config["destinationEmails"], config["domainTop"])
 
 # Call pipedMain on program entry:
 if __name__ == "__main__":
