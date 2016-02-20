271a2d309fb98458ffe76ce91c1bfdc628e7c6e4
krallice <krallice@github.com>
2016-02-20 22:07:17 +1100

    Flattened directory structure

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ touch /var/lib/piped/piped.cache
 chown -Rv root.root /var/lib/piped
 
 # Copy Files:
-cp -pv bin/piped /usr/local/bin/
+cp -pv piped /usr/local/bin/
 chown -v root.root /usr/local/bin/piped
 
-cp -pv etc/* /usr/local/etc/
-chown -v root.root /usr/local/etc/piped*conf
+cp -pv *yaml /usr/local/etc/
+chown -v root.root /usr/local/etc/piped*yaml
 
 # Copy systemd unit file:
 cp -pv piped.service /etc/systemd/system/
